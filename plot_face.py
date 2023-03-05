d6caae2372abe8af2092a2152625e1f5dc52612a
jamie082 <jamiemorrissey02@gmail.com>
2023-03-04 22:26:19 -0800

    added sharpening with NumPy and SciPy

--- plot_face.py
+++ plot_face.py
@@ -9,16 +9,29 @@ Small example to plot a racoon face.
 from scipy import misc,ndimage
 import imageio
 img = misc.face()
+img_2 = misc.face(gray=True).astype(float)
 imageio.imsave('face.png', img)
 import matplotlib.pyplot as plt
 
 # use filter
-blur_G = ndimage.gaussian_filter(img,sigma=7)
+blur_G = ndimage.gaussian_filter(img_2,sigma=7)
 rotate_face = ndimage.rotate(img, 45)
+blur = ndimage.gaussian_filter(img, 5)
 
+# one filter
 plt.subplot(131)
 plt.imshow(blur_G, cmap=plt.cm.gray)
 
+# two filter
+
 plt.subplot(132)
 plt.imshow(rotate_face, cmap=plt.cm.gray, vmin=30, vmax=200)
+
+# three filter
+blur_G = ndimage.gaussian_filter(blur, 1)
+alpha = 30
+plt.subplot(133)
+sharp = blur+alpha*(blur-blur_G)
+
+plt.imshow(sharp)
 plt.show()
